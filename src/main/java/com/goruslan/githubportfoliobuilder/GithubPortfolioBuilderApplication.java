f4fe44c1dbc93c2ba880f7bab9abed74d710456b
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-08-14 15:02:23 -0400

    Initial commit

---
+++ src/main/java/com/goruslan/githubportfoliobuilder/GithubPortfolioBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.goruslan.githubportfoliobuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPortfolioBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubPortfolioBuilderApplication.class, args);
+    }
+
+}
